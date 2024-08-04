50e05a04c4779cc129e21c24717565e0597738d2
Hwan Soo Kang <70050528+ai7dnn@users.noreply.github.com>
2024-08-04 13:57:23 +0900

    Add files via upload

--- R lect inclass/Ch12/12-1.R
+++ R lect inclass/Ch12/12-1.R
@@ -1,7 +1,11 @@
 mydata <- iris[,1:4]    # 데이터 준비
+head(mydata)
 
+mydata <- iris[,1:4]    # 데이터 준비
 fit <- kmeans(x=mydata, centers=3)
 fit
+# class(fit)
+
 fit$cluster             # 각 데이터에 대한 군집 번호
 fit$centers             # 각 군집의 중심점 좌표
 
@@ -10,5 +14,24 @@ library(cluster)
 clusplot(mydata, fit$cluster, color=TRUE, shade=TRUE,
          labels=2, lines=0)
 
+clusplot(mydata, fit$cluster, color=TRUE, shade=TRUE,
+         labels=1, lines=0)
+
+clusplot(mydata, fit$cluster, color=TRUE, shade=TRUE,
+         labels=1, lines=1)
+
+clusplot(mydata, fit$cluster, color=TRUE, shade=TRUE,
+         labels=2, lines=1)
+
 # 데이터에서 두 번째 군집의 데이터만 추출
 subset(mydata, fit$cluster==2)
+subset(mydata, fit$cluster==1)
+subset(mydata, fit$cluster==3)
+
+############## 정확도(accuracy) 계산
+iris.sp = iris$Species
+levels(iris.sp) = c(1, 2, 3)
+iris.sp
+
+mean(iris.sp == fit$cluster)
+
--- R lect inclass/Ch12/12-3.R
+++ R lect inclass/Ch12/12-3.R
@@ -2,16 +2,51 @@ install.packages("class")
 library(class)
 
 # 훈련용 데이터와 테스트용 데이터 준비
-tr.idx <- c(1:25,51:75, 101:125)    # 훈련용 데이터의 인덱스
+tr.idx <- c(1:25, 51:75, 101:125)   # 훈련용 데이터의 인덱스
 ds.tr <- iris[tr.idx, 1:4]          # 훈련용 데이터셋
+ds.tr
 ds.ts <- iris[-tr.idx, 1:4]         # 테스트용 데이터셋
-cl.tr <- factor(iris[tr.idx, 5])    # 훈련용 데이터셋의 그룹(품종) 정보
-cl.ts <- factor(iris[-tr.idx, 5])   # 테스트용 데이터셋의 그룹(품종) 정보
+ds.ts
 
+cl.tr <- factor(iris[tr.idx, 5])    # 훈련용 데이터셋의 그룹(품종) 정보
+cl.tr
+cl.ts <- factor(iris[-tr.idx, 5])   # 테스트용 데이터셋의 그룹(품종) 정보
+cl.ts
+
+####################################################
 pred <- knn(ds.tr, ds.ts, cl.tr, k=3, prob=TRUE)
 pred
 
 acc <- mean(pred==cl.ts)            # 예측 정확도
 acc
 
-table(pred,cl.ts)                   # 예측값과 실제값 비교 통계
+table(pred, cl.ts)                   # 예측값과 실제값 비교 통계
+
+##################################################
+pred <- knn(ds.tr, ds.ts, cl.tr, k=5, prob=TRUE)
+pred
+
+acc <- mean(pred==cl.ts)            # 예측 정확도
+acc
+
+table(pred, cl.ts)                   # 예측값과 실제값 비교 통계
+
+##################################################
+pred <- knn(ds.tr, ds.ts, cl.tr, k=7, prob=TRUE)
+pred
+
+acc <- mean(pred==cl.ts)            # 예측 정확도
+acc
+
+table(pred, cl.ts)                   # 예측값과 실제값 비교 통계
+
+###################################
+class(iris3)
+
+train <- rbind(iris3[1:25,,1], iris3[1:25,,2], iris3[1:25,,3])
+test <- rbind(iris3[26:50,,1], iris3[26:50,,2], iris3[26:50,,3])
+cl <- factor(c(rep("s",25), rep("c",25), rep("v",25)))
+cl
+knn(train, test, cl, k = 3, prob=TRUE)
+attributes(.Last.value)
+
